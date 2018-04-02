e694472d1062c13c442aeefd4603a0cc7bed2b87
Nate Sage <davidsage@Davids-MacBook-Pro.local>
2018-04-02 16:42:50 -0700

    Verify user credentials

--- client/components/login/LoginForm.js
+++ client/components/login/LoginForm.js
@@ -54,7 +54,7 @@ class LoginForm extends React.Component {
             <form onSubmit={this.onSubmit}>
                 <h1>Login</h1>
 
-                {/* { errors.form && <div className="alert alert-dnager">{errors.form}</div>} */}
+                { errors.form && <div className="alert alert-danger">{errors.form}</div>}
 
                 <TextFieldGroup
                 field="identifier"
